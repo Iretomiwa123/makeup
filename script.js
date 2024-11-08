6463fecaab0885ddb12ab711d85fa4d7a96b19c6
DFS0507 <adeyemooluwatoyin2018@gmail.com>
2024-11-08 09:13:34 +0100

    lysa

--- script.js
+++
@@ -1,5 +0,0 @@
-
-  function toggleModal() {
-    const navbarModal = document.getElementById("navbarModal");
-    navbarModal.style.display = navbarModal.style.display === "block" ? "none" : "block";
-  }
